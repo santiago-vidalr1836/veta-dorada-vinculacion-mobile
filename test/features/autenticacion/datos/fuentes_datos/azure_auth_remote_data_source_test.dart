07bb0ca2f91cb27ab474a4e06b3146caa12b85d8
santiago-vidalr1836 <santiago.vidalr@outlook.com>
2025-08-06 00:26:50 -0500

    test: add logout tests for azure auth

--- test/features/autenticacion/datos/fuentes_datos/azure_auth_remote_data_source_test.dart
+++ test/features/autenticacion/datos/fuentes_datos/azure_auth_remote_data_source_test.dart
@@ -1,55 +1,90 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:veta_dorada_vinculacion_mobile/features/autenticacion/datos/fuentes_datos/azure_auth_remote_data_source.dart';
 
-class _FakePca {
+class _FakeAppAuth {
   bool shouldThrow = false;
-  String token = 'token';
+  String accessToken = 'accessToken';
+  String refreshToken = 'refreshToken';
+  String idToken = 'idToken';
+  String? lastRefreshToken;
 
-  Future<String> acquireTokenInteractive({required List<String> scopes}) async {
+  Future<AuthorizationTokenResponse> authorizeAndExchangeCode(
+    AuthorizationTokenRequest request,
+  ) async {
     if (shouldThrow) throw Exception('error');
-    return token;
+    return AuthorizationTokenResponse(
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+      idToken: idToken,
+    );
   }
 
-  Future<String> acquireTokenSilent({required List<String> scopes}) async {
+  Future<TokenResponse> token(TokenRequest request) async {
+    lastRefreshToken = request.refreshToken;
     if (shouldThrow) throw Exception('error');
-    return token;
+    if (request.refreshToken == null) throw Exception('missing refresh token');
+    return TokenResponse(
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    );
   }
 
-  Future<void> logout() async {
+  Future<void> endSession(EndSessionRequest request) async {
     if (shouldThrow) throw Exception('error');
   }
 }
 
 void main() {
   group('AzureAuthRemoteDataSource', () {
-    late _FakePca fakePca;
+    late _FakeAppAuth fakeAppAuth;
     late AzureAuthRemoteDataSource dataSource;
 
-    setUp(() async {
-      fakePca = _FakePca();
-      dataSource = await AzureAuthRemoteDataSource.create(pca: fakePca);
+    setUp(() {
+      fakeAppAuth = _FakeAppAuth();
+      dataSource = AzureAuthRemoteDataSource.create(appAuth: fakeAppAuth);
     });
 
     test('login returns token on success', () async {
-      fakePca.shouldThrow = false;
+      fakeAppAuth.shouldThrow = false;
       final token = await dataSource.login();
-      expect(token, fakePca.token);
+      expect(token, fakeAppAuth.accessToken);
     });
 
     test('login throws AzureAuthException on failure', () async {
-      fakePca.shouldThrow = true;
+      fakeAppAuth.shouldThrow = true;
       expect(dataSource.login, throwsA(isA<AzureAuthException>()));
     });
 
     test('refreshToken returns token on success', () async {
-      fakePca.shouldThrow = false;
+      fakeAppAuth.shouldThrow = false;
+      await dataSource.login();
+      fakeAppAuth.accessToken = 'newAccess';
+      fakeAppAuth.refreshToken = 'newRefresh';
       final token = await dataSource.refreshToken();
-      expect(token, fakePca.token);
+      expect(token, 'newAccess');
+      expect(fakeAppAuth.lastRefreshToken, 'refreshToken');
     });
 
     test('refreshToken throws AzureAuthException on failure', () async {
-      fakePca.shouldThrow = true;
+      fakeAppAuth.shouldThrow = false;
+      await dataSource.login();
+      fakeAppAuth.shouldThrow = true;
       expect(dataSource.refreshToken, throwsA(isA<AzureAuthException>()));
+    });
+
+    test('logout completes successfully and clears tokens', () async {
+      fakeAppAuth.shouldThrow = false;
+      await dataSource.login();
+      await dataSource.logout();
+      expect(dataSource.refreshToken, throwsA(isA<AzureAuthException>()));
+    });
+
+    test('logout throws AzureAuthException when endSession fails', () async {
+      fakeAppAuth.shouldThrow = false;
+      await dataSource.login();
+      fakeAppAuth.shouldThrow = true;
+      expect(dataSource.logout, throwsA(isA<AzureAuthException>()));
     });
   });
 }
